5fe6f27ab28b9fd7fb43f31d7654840900f394c9
Alexandru Dochioiu <alexandrudochioiu@Alexs-MB-2025.local>
2025-07-01 09:32:00 +0300

    Removed freezed dependency

--- lib/src/models/signature_request_data.dart
+++ lib/src/models/signature_request_data.dart
@@ -1,16 +1,34 @@
 // ignore_for_file: invalid_annotation_target
 
 import "package:cardano_dart_types/cardano_dart_types.dart";
-import "package:freezed_annotation/freezed_annotation.dart";
 import "package:ledger_cardano_plus/ledger_cardano_plus_models.dart";
+import "package:meta/meta.dart";
 
-part "signature_request_data.freezed.dart";
 
-@freezed
-sealed class SignatureRequestData with _$SignatureRequestData {
-  const factory SignatureRequestData({
-    required ParsedSigningRequest ledgerSignRequest,
-    required CardanoPubAccount ledgerPubAccount,
-  }) = _SignatureRequestData;
-  const SignatureRequestData._();
+@immutable
+class SignatureRequestData {
+  final ParsedSigningRequest ledgerSignRequest;
+  final CardanoPubAccount ledgerPubAccount;
+
+  const SignatureRequestData({
+    required this.ledgerSignRequest,
+    required this.ledgerPubAccount,
+  });
+
+  @override
+  String toString() {
+    return "SignatureRequestData(ledgerSignRequest: $ledgerSignRequest, ledgerPubAccount: $ledgerPubAccount)";
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is SignatureRequestData &&
+            (identical(other.ledgerSignRequest, ledgerSignRequest) || other.ledgerSignRequest == ledgerSignRequest) &&
+            (identical(other.ledgerPubAccount, ledgerPubAccount) || other.ledgerPubAccount == ledgerPubAccount));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, ledgerSignRequest, ledgerPubAccount);
 }
